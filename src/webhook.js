d0cb49a82a53705ac0dcf45e899b4e50d8cc0e43
tschuhmacher <t.schuhmacher+smb@gmail.com>
2018-05-24 18:54:03 +0200

    webhook ausgelagert

--- src/webhook.js
+++ src/webhook.js
@@ -32,3 +32,38 @@ router.get('/webhook', (req, res) => {
     }
   }
 });
+
+// Accepts POST requests at /webhook endpoint
+router.post('/webhook', (req, res) => {
+  // Parse the request body from the POST
+  const data = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (data.object === 'page') {
+    // Iterate over each entry
+    //there may be multiple if batched
+    data.entry.forEach((pageEntry) => {
+      // Iterate over each messaging event and handle accordingly
+      pageEntry.messaging.forEach((messagingEvent) => {
+        console.log({messagingEvent});
+        if (messagingEvent.message) {
+          console.log("message!");
+        } else if (messagingEvent.postback) {
+          console.log("postback!");
+        } else {
+          console.log(
+            'Webhook received unknown messagingEvent: ',
+            messagingEvent
+          );
+        }
+      });
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+});
