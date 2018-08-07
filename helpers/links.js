55ba355e046bf1e66133cf9e08c1ea4e874548f5
tschuhmacher <t.schuhmacher+smb@gmail.com>
2018-08-07 17:27:23 +0200

    test function

--- helpers/links.js
+++ helpers/links.js
@@ -1,15 +1,15 @@
-const buyTicketLink = [
-  {
+const buildLinks = (linkText, linkUrl, linkTitle) => {
+  return {
     attachment:{
       type: "template",
       payload: {
         template_type: "button",
-        text: "Online unter:",
+        text: linkText,
         buttons: [
           {
             type:"web_url",
-            url:"https://shop.smb.museum/#/tickets/list?date=2018-02-22&museum_id=27&ticketSelection=%5Bobject%20Object%5D",
-            title:"Tickets",
+            url: linkUrl,
+            title: linkTitle,
             webview_height_ratio: "tall",
             messenger_extensions: "false",
           }
@@ -17,7 +17,10 @@ const buyTicketLink = [
       }
     }
   }
-];
+}
+
+const buyTicketLink = buildLinks("Online unter:", "https://shop.smb.museum/#/tickets/list?date=2018-02-22&museum_id=27&ticketSelection=%5Bobject%20Object%5D", "Tickets");
+
 
 exports.link = {
   buyTicketLink
